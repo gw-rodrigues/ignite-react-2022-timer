cf81433d07c31d5573aff62e4f0402b44d480418
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-24 17:53:10 +0200

    feat: default theme and applying theme props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { ThemeProvider } from 'styled-components';
 import { Button } from './components/Button';
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button />
       <Button variant="success" />
       <Button variant="danger" />
       <Button variant="secondary" />
-    </>
+    </ThemeProvider>
   );
 }
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,9 +17,17 @@ export const ButtonContainer = styled.button<IButtonContainerProps>`
   width: 100px;
   height: 40px;
 
+  border-radius: 8px;
+  margin-right: 1rem;
+
+  background: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.white};
+
+  /*
   ${(props) => {
     return css`
       background-color: ${buttonVariants[props.variant]};
     `;
   }}
+  */
 `;
