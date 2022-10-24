81d2f060b100b628299b42df20bd00a7b9463316
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-24 18:15:51 +0200

    fix: removing text

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export function App() {
       <Button variant="success" />
       <Button variant="danger" />
       <Button variant="secondary" />
-      dwadwad
+
       <GlobalStyle />
     </ThemeProvider>
   );
