0587821911607d651028fdcbaa39101535392546
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-25 10:03:22 +0200

    feat: importing Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
@@ -6,7 +7,7 @@ import { defaultTheme } from './styles/themes/default'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-
+      <Router />
       <GlobalStyle />
     </ThemeProvider>
   )
