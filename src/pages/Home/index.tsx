1744f3ca0c8431a1acc4f3406f9996cc9c50d7c4
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-25 10:57:10 +0200

    feat: timer elements

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,32 @@
+import { Play } from 'phosphor-react'
+
 export function Home() {
-  return <div>Home</div>
+  return (
+    <div>
+      <form action="">
+        <div>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="minutesAmount"></label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </div>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </div>
+  )
 }
