11c3263d1499ed66b2ea767bf8bf55d8d40aa80b
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-25 09:59:26 +0200

    remove: components button and styles

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, TButtonVariant } from './Button.styles'
-
-interface IButtonVariants {
-  variant?: TButtonVariant
-}
-
-export function Button({ variant = 'primary' }: IButtonVariants) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
