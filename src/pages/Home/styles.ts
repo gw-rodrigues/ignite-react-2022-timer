59e661051b645d589fc752250a76734dd185d898
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-25 11:24:54 +0200

    style: add shadow

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -41,6 +41,8 @@ export const CountDownContainer = styled.div`
     background: ${(props) => props.theme['gray-700']};
     padding: 2rem 1rem;
     border-radius: 8px;
+
+    box-shadow: 5px 8px 8px -8px black;
   }
 `
 
@@ -52,4 +54,32 @@ export const Separator = styled.div`
   overflow: hidden;
   display: flex;
   justify-content: center;
+`
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 0.5rem;
+  font-weight: bold;
+
+  cursor: pointer;
+
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-700']};
+  }
 `
