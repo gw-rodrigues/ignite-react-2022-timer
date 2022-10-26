808826b3e54a312b04e1c6bbda4a685c89751810
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-26 10:00:25 +0200

    feat: cycles context, interface, methods, provider

---
+++ src/contexts/CycleContext.tsx
@@ -0,0 +1,113 @@
+import { createContext, ReactNode, useState } from 'react'
+
+interface ICreateCycleData {
+  task: string
+  minutesAmount: number
+}
+
+interface ICycle {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date
+  interruptDate?: Date
+  finishedDate?: Date
+}
+
+interface ICyclesContextType {
+  cycles: ICycle[]
+  activeCycle: ICycle | undefined
+  activeCycleId: string | null
+  amountSecondsPassed: number
+  markCurrentCycleAsFinished: () => void
+  setSecondsPassed: (seconds: number) => void
+  createNewCycle: (data: ICreateCycleData) => void
+  interruptCurrentCycle: () => void
+}
+
+interface ICycleContextProvider {
+  children: ReactNode
+}
+
+/**
+ * Contexto que vai ser usado para obter as variáveis e funções
+ * const {...} = useContext(CyclesContext)
+ */
+export const CyclesContext = createContext({} as ICyclesContextType)
+
+/**
+ * O provider do contexto em vez usar <CycleContext.Provider> no componente.
+ * Criamos aqui um método com <CycleContext.Provider> e adicionamos as variáveis e funções, para nao sair logica do contexto.
+ * Usamos essa função a volta do componentes que vamos partilhar.
+ * Então passamos os componentes que vao usar contexto como children.
+ * <CycleContextProvider> -> função com context.provider
+ *    <Router /> -> children (home/history/etc...)
+ * </CycleContextProvider>
+ */
+export function CycleContextProvider({ children }: ICycleContextProvider) {
+  const [cycles, setCycles] = useState<ICycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds)
+  }
+
+  function markCurrentCycleAsFinished() {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, finishedDate: new Date() }
+        } else {
+          return cycle
+        }
+      }),
+    )
+  }
+
+  function createNewCycle(data: ICreateCycleData) {
+    const id = String(new Date().getTime())
+    const newCycle: ICycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    }
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+    setAmountSecondsPassed(0)
+
+    // reset()
+  }
+
+  function interruptCurrentCycle() {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, interruptDate: new Date() }
+        } else {
+          return cycle
+        }
+      }),
+    )
+    setActiveCycleId(null)
+  }
+
+  return (
+    <CyclesContext.Provider
+      value={{
+        cycles,
+        activeCycle,
+        activeCycleId,
+        markCurrentCycleAsFinished,
+        amountSecondsPassed,
+        setSecondsPassed,
+        createNewCycle,
+        interruptCurrentCycle,
+      }}
+    >
+      {children}
+    </CyclesContext.Provider>
+  )
+}
