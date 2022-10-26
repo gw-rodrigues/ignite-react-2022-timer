bad3c79511b99345a12d87e62421cd298700463e
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-26 15:34:22 +0200

    feat: use produce methods add, interrupt, finished

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -24,13 +24,32 @@ interface ICycleState {
 export function cyclesReducer(state: ICycleState, action: any) {
   switch (action.type) {
     case EActionsTypes.ADD_NEW_CYCLE:
+      /*
+      //Código antigo
       return {
         ...state,
         cycles: [...state.cycles, action.payload.newCycle],
         activeCycleId: action.payload.newCycle.id,
       }
+      */
+      /**
+       * Produce - Immer -> recebe vários parâmetros.
+       *
+       * param 1 -> qual state queremos modificar
+       * param 2 -> função, (draft)- o rascunho que ira ser
+       * modificado final, mesmo tipagem que o param 1 - state.
+       *
+       * sendo assim, no draft podemos usar métodos "push" ou "="
+       * para definir os valores.
+       */
+      return produce(state, (draft) => {
+        draft.cycles.push(action.payload.newCycle)
+        draft.activeCycleId = action.payload.newCycle.id
+      })
 
-    case EActionsTypes.INTERRUPT_CURRENT_CYCLE:
+    case EActionsTypes.INTERRUPT_CURRENT_CYCLE: {
+      /*
+      //Código antigo
       return {
         ...state,
         cycles: state.cycles.map((cycle) => {
@@ -42,8 +61,22 @@ export function cyclesReducer(state: ICycleState, action: any) {
         }),
         activeCycleId: null,
       }
+      */
+      const currentCycleIndex = state.cycles.findIndex(
+        (cycle) => cycle.id === state.activeCycleId,
+      )
 
-    case EActionsTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
+      if (currentCycleIndex < 0) return state
+
+      return produce(state, (draft) => {
+        draft.activeCycleId = null
+        draft.cycles[currentCycleIndex].interruptDate = new Date()
+      })
+    }
+
+    case EActionsTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
+      /*
+      //Código antigo 
       return {
         ...state,
         cycles: state.cycles.map((cycle) => {
@@ -54,7 +87,19 @@ export function cyclesReducer(state: ICycleState, action: any) {
           }
         }),
         activeCycleId: null,
-      }
+      } 
+      */
+      const currentCycleIndex = state.cycles.findIndex(
+        (cycle) => cycle.id === state.activeCycleId,
+      )
+
+      if (currentCycleIndex < 0) return state
+
+      return produce(state, (draft) => {
+        draft.activeCycleId = null
+        draft.cycles[currentCycleIndex].finishedDate = new Date()
+      })
+    }
 
     default:
       return state
