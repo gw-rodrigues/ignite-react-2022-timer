028cd34676cc9ca671a8bbc53bbf7e3e1a8aa516
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-26 15:14:54 +0200

    feat: reducer methods

---
+++ src/reducers/cycles/reducer.ts
@@ -0,0 +1,55 @@
+import { EActionsTypes } from './actions'
+
+export interface ICycle {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date
+  interruptDate?: Date
+  finishedDate?: Date
+}
+
+interface ICycleState {
+  cycles: ICycle[]
+  activeCycleId: string | null
+}
+
+export function cyclesReducer(state: ICycleState, action: any) {
+  switch (action.type) {
+    case EActionsTypes.ADD_NEW_CYCLE:
+      return {
+        ...state,
+        cycles: [...state.cycles, action.payload.newCycle],
+        activeCycleId: action.payload.newCycle.id,
+      }
+
+    case EActionsTypes.INTERRUPT_CURRENT_CYCLE:
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, interruptDate: new Date() }
+          } else {
+            return cycle
+          }
+        }),
+        activeCycleId: null,
+      }
+
+    case EActionsTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, finishedDate: new Date() }
+          } else {
+            return cycle
+          }
+        }),
+        activeCycleId: null,
+      }
+
+    default:
+      return state
+  }
+}
